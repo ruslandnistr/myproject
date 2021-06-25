609cdbf29273ed14856fe1cfbb3602a31ffa01bb
ruslandnistr <rruslandnistr@gmail.com>
2021-06-25 14:26:38 +0000

    Star

--- js/script.js
+++ js/script.js
@@ -8,3 +8,30 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+function upDateClock(){
+  const clockContainer = document.querySelector('.clock');
+clockContainer.innerHTML = new Date().toLocaleString('en-GB');
+};
+setInterval(upDateClock, 1000)
+
+
+//                                              Таймер знищення
+// const dalaySecs = 600;
+// const startTime = new Date();
+// function updateSelfDestruct(){
+//   const selfDestractContainer = document.querySelector('.self-destruct');
+//   const curentTime = new Date();
+//   const elepsedTimeSec = Math.round((curentTime - startTime) / 1000);
+//   const leftSecs = dalaySecs - elepsedTimeSec;
+//   selfDestractContainer.innerText = leftSecs;
+//   if(leftSecs < 1){
+//     document.querySelector('body').innerHTML = '';
+//   }
+// }
+// setInterval(updateSelfDestruct, 1000)
+
+
+// function start(div_h1 , x , y){
+//   document.querySelector(div_h1).textContent = x + y;
+// }
+// start('.div_h1', 3, 4)
