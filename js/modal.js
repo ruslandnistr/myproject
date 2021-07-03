d66e381a84d1a0ded4d1d8d905f40c3bd45a004b
ruslandnistr <rruslandnistr@gmail.com>
2021-07-03 14:53:39 +0300

    star

--- js/modal.js
+++ js/modal.js
@@ -1,36 +1,36 @@
-let modal = document.getElementById('myModal');
-let body = document.getElementsByClassName('body');
+// let modal = document.getElementById('myModal');
+// let body = document.getElementsByClassName('body');
 
-let buttonNo = document.getElementsByClassName("button-modal-no")[0];
-let buttonYes = document.getElementsByClassName("button-modal-yes")[0];
-btn = function() {
-    modal.style.display = "block";
-}
-btn();
+// let buttonNo = document.getElementsByClassName("button-modal-no")[0];
+// let buttonYes = document.getElementsByClassName("button-modal-yes")[0];
+// btn = function() {
+//     modal.style.display = "block";
+// }
+// btn();
 
-    buttonYes.addEventListener('click', ageYes)
-     function ageYes() {
-        modal.style.display = "none";
-}
-    buttonNo.addEventListener('click', ageNo) 
-         function ageNo() {
-            modal.style.display = "none";
-            const dalaySecs = 10;
-            const startTime = new Date();
-            function updateSelfDestruct(){
-            const selfDestractContainer = document.querySelector('.self-destruct');
-            const curentTime = new Date();
-            const elepsedTimeSec = Math.round((curentTime - startTime) / 1000);
-            const leftSecs = dalaySecs - elepsedTimeSec;
-            selfDestractContainer.innerText = leftSecs;
-            if(leftSecs < 1){
-            document.querySelector('body').innerHTML = '';
-    }
-    }
-setInterval(updateSelfDestruct, 1000)
-}
-Window.addEventListener("click", (event) => {
-    if (event.target == modal) {
-                body = "none";
-        } 
-    });
+//     buttonYes.addEventListener('click', ageYes)
+//      function ageYes() {
+//         modal.style.display = "none";
+// }
+//     buttonNo.addEventListener('click', ageNo) 
+//          function ageNo() {
+//             modal.style.display = "none";
+//             const dalaySecs = 10;
+//             const startTime = new Date();
+//             function updateSelfDestruct(){
+//             const selfDestractContainer = document.querySelector('.self-destruct');
+//             const curentTime = new Date();
+//             const elepsedTimeSec = Math.round((curentTime - startTime) / 1000);
+//             const leftSecs = dalaySecs - elepsedTimeSec;
+//             selfDestractContainer.innerText = leftSecs;
+//             if(leftSecs < 1){
+//             document.querySelector('body').innerHTML = '';
+//     }
+//     }
+// setInterval(updateSelfDestruct, 1000)
+// }
+// Window.addEventListener("click", (event) => {
+//     if (event.target == modal) {
+//                 body = "none";
+//         } 
+//     });
